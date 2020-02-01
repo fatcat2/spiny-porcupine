838dc9e2439923c2056e512d294a42659935775d
Ryan Chen <ryanjchen2@gmail.com>
2020-02-01 14:15:59 -0500

    New function

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from quart import Quart
 def index():
     return "WASSSSUPPP"
 
+def bot_practice_method():
+    x = 5
+    if x == 5:
+        return 7
+
 if __name__ == "__main__":
     app.run()
+    # asdffsd fsadfasfsadf
